0596f6f2b11c7193059ce1873b87e848c2cd0521
Reejo Samuel <m@reejosamuel.com>
2015-03-25 20:16:22 +0400

    Added fetching repository URLs

--- lib/github_trending/project.rb
+++ lib/github_trending/project.rb
@@ -1,10 +1,10 @@
 module Github
   module Trending
     class Project
-      attr_accessor :name, :lang, :description, :star_count
+      attr_accessor :name, :lang, :description, :star_count, :url
 
       def to_a
-        [@name, @lang, @star_count.to_s]
+        [@name, @lang, @star_count.to_s, @url]
       end
     end
   end
